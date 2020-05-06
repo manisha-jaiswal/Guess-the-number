e0bf53054d9a6e173fb15d35aedaa68a189bb8b7
MANISHA KUMARI <Manisha16041998@gmail.com>
2020-05-06 15:43:26 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+#find the number is same as the hidden number or notnumber of guases is limited max 9,
+# print number of guases left , game over,
+# if he won then print won and print number of guases
+
+n=18
+number_of_guesses=1
+print("Number of guesses is limited to only 9 times: ")
+while (number_of_guesses<=9):
+    guess_number = int(input("Guess the number :\n"))
+    if guess_number<18:
+        print("you enter less number please input greater number.\n")
+    elif guess_number>18:
+        print("you enter greater number please input smaller number.\n ")
+    else:
+        print("you won\n")
+        print(number_of_guesses,"no.of guesses he took to finish.")
+        break
+    print(9-number_of_guesses,"no. of guesses left")
+    number_of_guesses = number_of_guesses + 1
+
+if(number_of_guesses>9):
+    print("Game Over")
